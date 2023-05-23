96653300c855247d891193e281ab35542c074a9e
Yasakikondo74 <83692124+Yasakikondo74@users.noreply.github.com>
2023-05-23 19:19:10 +0700

    Add files via upload

---
+++ Main_page.js
@@ -0,0 +1,9 @@
+function scroll_left() {
+    var left = document.querySelector(".side_scroller");
+    left.scrollBy(-350, 0)
+}
+
+function scroll_right() {
+    var right = document.querySelector(".side_scroller");
+    right.scrollBy(350, 0)
+}
