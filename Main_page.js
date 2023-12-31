b8034faa72706879f1073975c3676398dc873807
Yasakikondo74 <83692124+Yasakikondo74@users.noreply.github.com>
2023-12-31 20:21:35 +0700

    base released
    update 2023/12/31

--- Main_page.js
+++ Main_page.js
@@ -1,9 +1,56 @@
-function scroll_left() {
-    var left = document.querySelector(".side_scroller");
-    left.scrollBy(-350, 0)
+let scrollAmount = 0;
+const imageList = document.querySelector('.image_list');
+const scrollIncrement = 15; // Increase this value to make the scroll faster
+let scrollInterval;
+
+function scrollLeft() {
+    if (scrollAmount > 0) {
+        scrollAmount -= scrollIncrement;
+        imageList.scrollTo({
+            left: scrollAmount,
+            behavior: 'auto'
+        });
+        scrollInterval = requestAnimationFrame(scrollLeft);
+    } else {
+        cancelAnimationFrame(scrollInterval);
+        document.querySelector('button:nth-child(1)').style.backgroundImage = "url('img/left_end.png')";
+    }
 }
 
-function scroll_right() {
-    var right = document.querySelector(".side_scroller");
-    right.scrollBy(350, 0)
+function scrollRight() {
+    if (scrollAmount < imageList.scrollWidth - imageList.clientWidth) {
+        scrollAmount += scrollIncrement;
+        imageList.scrollTo({
+            left: scrollAmount,
+            behavior: 'auto'
+        });
+        scrollInterval = requestAnimationFrame(scrollRight);
+    } else {
+        cancelAnimationFrame(scrollInterval);
+        document.querySelector('button:nth-child(2)').style.backgroundImage = "url('img/right_end.png')";
+    }
 }
+
+document.querySelector('button:nth-child(1)').addEventListener('mousedown', () => {
+    scrollInterval = requestAnimationFrame(scrollLeft);
+});
+
+document.querySelector('button:nth-child(1)').addEventListener('mouseup', () => {
+    cancelAnimationFrame(scrollInterval);
+});
+
+document.querySelector('button:nth-child(1)').addEventListener('mouseleave', () => {
+    cancelAnimationFrame(scrollInterval);
+});
+
+document.querySelector('button:nth-child(2)').addEventListener('mousedown', () => {
+    scrollInterval = requestAnimationFrame(scrollRight);
+});
+
+document.querySelector('button:nth-child(2)').addEventListener('mouseup', () => {
+    cancelAnimationFrame(scrollInterval);
+});
+
+document.querySelector('button:nth-child(2)').addEventListener('mouseleave', () => {
+    cancelAnimationFrame(scrollInterval);
+});
