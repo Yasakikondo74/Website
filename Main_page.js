952d8e8ad2489fc7615417207d116721b90123fb
Yasakikondo74 <83692124+Yasakikondo74@users.noreply.github.com>
2023-12-31 21:22:06 +0700

    finish

--- Main_page.js
+++ Main_page.js
@@ -1,56 +1,26 @@
-let scrollAmount = 0;
 const imageList = document.querySelector('.image_list');
-const scrollIncrement = 15; // Increase this value to make the scroll faster
+const leftButton = document.querySelector('.controls button:nth-child(1)');
+const rightButton = document.querySelector('.controls button:nth-child(2)');
+
+let scrollSpeed = 15; // Adjust this value to change the scroll speed
 let scrollInterval;
 
-function scrollLeft() {
-    if (scrollAmount > 0) {
-        scrollAmount -= scrollIncrement;
-        imageList.scrollTo({
-            left: scrollAmount,
-            behavior: 'auto'
-        });
-        scrollInterval = requestAnimationFrame(scrollLeft);
-    } else {
-        cancelAnimationFrame(scrollInterval);
-        document.querySelector('button:nth-child(1)').style.backgroundImage = "url('img/left_end.png')";
-    }
-}
-
-function scrollRight() {
-    if (scrollAmount < imageList.scrollWidth - imageList.clientWidth) {
-        scrollAmount += scrollIncrement;
-        imageList.scrollTo({
-            left: scrollAmount,
-            behavior: 'auto'
-        });
-        scrollInterval = requestAnimationFrame(scrollRight);
-    } else {
-        cancelAnimationFrame(scrollInterval);
-        document.querySelector('button:nth-child(2)').style.backgroundImage = "url('img/right_end.png')";
-    }
-}
-
-document.querySelector('button:nth-child(1)').addEventListener('mousedown', () => {
-    scrollInterval = requestAnimationFrame(scrollLeft);
+leftButton.addEventListener('mousedown', () => {
+    scrollInterval = setInterval(() => {
+        imageList.scrollLeft -= scrollSpeed;
+    }, 20);
 });
 
-document.querySelector('button:nth-child(1)').addEventListener('mouseup', () => {
-    cancelAnimationFrame(scrollInterval);
+leftButton.addEventListener('mouseup', () => {
+    clearInterval(scrollInterval);
 });
 
-document.querySelector('button:nth-child(1)').addEventListener('mouseleave', () => {
-    cancelAnimationFrame(scrollInterval);
+rightButton.addEventListener('mousedown', () => {
+    scrollInterval = setInterval(() => {
+        imageList.scrollLeft += scrollSpeed;
+    }, 20);
 });
 
-document.querySelector('button:nth-child(2)').addEventListener('mousedown', () => {
-    scrollInterval = requestAnimationFrame(scrollRight);
-});
-
-document.querySelector('button:nth-child(2)').addEventListener('mouseup', () => {
-    cancelAnimationFrame(scrollInterval);
-});
-
-document.querySelector('button:nth-child(2)').addEventListener('mouseleave', () => {
-    cancelAnimationFrame(scrollInterval);
+rightButton.addEventListener('mouseup', () => {
+    clearInterval(scrollInterval);
 });
